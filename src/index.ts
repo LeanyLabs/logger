284dd34378622b63246454516f7eefda8fee41c6
Vova <vovadorko@gmail.com>
2021-07-16 15:53:54 +0300

    feat: initial logger implementation

---
+++ src/index.ts
@@ -0,0 +1,84 @@
+import { createLogger as create, format, transports } from "winston";
+import { consoleFormat } from "winston-console-format";
+
+interface defaultLoggerParams {
+  LOGGING_LEVEL: string | undefined;
+  SERVICE_NAME?: string | undefined;
+}
+
+interface createLoggerParams {
+  isProd: boolean;
+  loggingLevel: string;
+  serviceNeme: string;
+}
+
+const NODE_ENV = process.env.NODE_ENV;
+const LOGGING_LEVEL = process.env.LOGGING_LEVEL;
+const SERVICE_NAME = process.env.SERVICE_NAME;
+
+let isProd = NODE_ENV?.includes("prod");
+
+function createProdLogger({ LOGGING_LEVEL }: defaultLoggerParams) {
+  return create({
+    level: LOGGING_LEVEL,
+    format: format.combine(
+      format.timestamp(),
+      format.ms(),
+      format.errors({ stack: true }),
+      format.splat(),
+      format.json()
+    ),
+    transports: [new transports.Console()],
+  });
+}
+
+function createDevLogger({ LOGGING_LEVEL, SERVICE_NAME }: defaultLoggerParams) {
+  return create({
+    level: LOGGING_LEVEL,
+    format: format.combine(
+      format.timestamp(),
+      format.ms(),
+      format.errors({ stack: true }),
+      format.splat(),
+      format.json()
+    ),
+    defaultMeta: { service: SERVICE_NAME },
+    transports: [
+      new transports.Console({
+        format: format.combine(
+          format.colorize({ all: true }),
+          format.padLevels(),
+          consoleFormat({
+            showMeta: true,
+            metaStrip: ["timestamp", "service"],
+            inspectOptions: {
+              depth: 3,
+              colors: true,
+              maxArrayLength: 10,
+              breakLength: 120,
+              compact: Infinity,
+            },
+          })
+        ),
+      }),
+    ],
+  });
+}
+
+export function createLogger({
+  isProd,
+  loggingLevel,
+  serviceNeme,
+}: createLoggerParams) {
+  return isProd
+    ? createProdLogger({ LOGGING_LEVEL: loggingLevel })
+    : createDevLogger({
+        SERVICE_NAME: serviceNeme,
+        LOGGING_LEVEL: loggingLevel,
+      });
+}
+
+export const logger = isProd
+  ? createProdLogger({ LOGGING_LEVEL })
+  : createDevLogger({ SERVICE_NAME, LOGGING_LEVEL });
+export default logger;
