c11d5dbd7cd7ff12b5849a0931ccda1e809f639e
Vova <vovadorko@gmail.com>
2021-07-19 10:53:48 +0300

    feat: errot object logging in json

--- src/index.ts
+++ src/index.ts
@@ -19,31 +19,23 @@ const SERVICE_NAME = process.env.SERVICE_NAME;
 let isProd = NODE_ENV?.includes("prod");
 
 function createProdLogger({ LOGGING_LEVEL }: defaultLoggerParams) {
+  function errorReplacer(key: any, value: { message: any; stack: any }) {
+    if (value instanceof Error) {
+      return { message: value.message, stack: value.stack };
+    }
+    return value;
+  }
+
+  const logFormat = format.printf((info) => {
+    return `${JSON.stringify(info, errorReplacer)}`;
+  });
+
   return create({
     level: LOGGING_LEVEL,
-    format: format.combine(
-      format.timestamp(),
-      format.ms(),
-      format.errors({ stack: true }),
-      format.splat(),
-      format.json()
-    ),
+    format: format.combine(format.ms(), format.timestamp(), logFormat),
     transports: [
       new transports.Console({
-        format: format.combine(
-          //   format.padLevels(),
-          consoleFormat({
-            showMeta: true,
-            metaStrip: ["timestamp", "service"],
-            inspectOptions: {
-              depth: 3,
-              colors: true,
-              maxArrayLength: 10,
-              breakLength: 120,
-              compact: Infinity,
-            },
-          })
-        ),
+        format: format.combine(format.ms(), format.timestamp(), logFormat),
       }),
     ],
   });
